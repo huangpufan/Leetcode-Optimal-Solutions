8490e4c95fed0d3ac43cc63803ebf8e803564ae3
hpf <59730801@qq.com>
2022-05-10 15:37:46 +0800

    新建 AllSolutions 文件夹，确定 Solution 文件存放位置。

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "SolutionCommon.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    vector<int> res = {};
+    vector<int>nums={1,2,3,5};
+    unordered_map<int, int> numLocation;
+    for (size_t i = 0; i < res.size(); i++) {
+        numLocation.insert(make_pair(nums))
+        cout<<nums[i]<<i;
+    }
+
 }
