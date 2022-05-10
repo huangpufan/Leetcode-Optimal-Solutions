7271626a6dfcc12b17a7b342585cb4186dd73ec9
hpf <59730801@qq.com>
2022-05-10 09:57:39 +0800

    新建 AllSolutions 文件夹，确定 Solution 文件存放位置。

--- AllSolutions/Solution0001.cpp
+++ AllSolutions/Solution0001.cpp
@@ -2,7 +2,7 @@
 // Created by hpf on 22-5-10.
 //
 
-#include "SolutionCommon.h"
+#include "../SolutionCommon.h"
 
 /*
  * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那两个整数，并返回它们的数组下标。
@@ -18,3 +18,9 @@ public:
 
     }
 };
+/*
+ * 经典两数之和，任何人上力扣见到的第一题。
+ *
+ * 方法也是经典双指针。
+ *
+ */
