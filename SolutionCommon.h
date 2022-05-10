e9031e99f6bfc5c178b99b7b677756fcd6f63da2
hpf <59730801@qq.com>
2022-05-10 09:38:48 +0800

    测试提交(恶意提交刷 commit！

---
+++ SolutionCommon.h
@@ -0,0 +1,17 @@
+//
+// Created by hpf on 22-5-10.
+//
+
+#ifndef LEETCODE_OPTIMAL_SOLUTIONS_SOLUTIONCOMMON_H
+#define LEETCODE_OPTIMAL_SOLUTIONS_SOLUTIONCOMMON_H
+
+#endif //LEETCODE_OPTIMAL_SOLUTIONS_SOLUTIONCOMMON_H
+
+#include <vector>
+#include <cstring>
+#include <cstdio>
+#include "iostream"
+#include "cmath"
+#include<bits/stdc++.h>
+
+using namespace std;
