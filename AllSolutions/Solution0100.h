6f72ec8a750eaa2d2f14fd89f606d87ecca6db08
hpf <59730801@qq.com>
2022-05-12 20:00:46 +0800

    完成 0100 两树相同的判断。

---
+++ AllSolutions/Solution0100.h
@@ -0,0 +1,31 @@
+//
+// Created by hpf on 22-5-12.
+//
+
+#ifndef LEETCODE_OPTIMAL_SOLUTIONS_SOLUTION0100_H
+#define LEETCODE_OPTIMAL_SOLUTIONS_SOLUTION0100_H
+
+#include "../SolutionCommon.h"
+
+class Solution {
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q) {
+        if (p == nullptr && q == nullptr)
+            return true;
+        if(!(p!= nullptr&&q!= nullptr)){
+            return false;
+        }
+        if(p->val==q->val){
+            return isSameTree(p->left,q->left)&& isSameTree(p->right,q->right);
+        }else{
+            return false;
+        }
+    }
+};
+
+// 这题也很简单，不用写啥，直接递归很舒服。
+// 但是如果不递归的话，我还没有立刻将任何一种递归转化为非递归的能力，这道题想一想吧。
+// 我感觉其实任何一种遍历方式，输出个字符串或者数组，然后做比较就可以。
+// BFS DFS 先序中序后序 。而且不能使用递归，不然就和这个方法没啥区别了。
+
+#endif //LEETCODE_OPTIMAL_SOLUTIONS_SOLUTION0100_H
--- SolutionCommon.h
+++ SolutionCommon.h
@@ -6,7 +6,6 @@
 #define LEETCODE_OPTIMAL_SOLUTIONS_SOLUTIONCOMMON_H
 
 
-
 #include <vector>
 #include <cstring>
 #include <cstdio>
@@ -21,7 +20,22 @@ struct ListNode {
     ListNode *next;
 
     ListNode() : val(0), next(nullptr) {}
+
     ListNode(int x) : val(x), next(nullptr) {}
+
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 #endif //LEETCODE_OPTIMAL_SOLUTIONS_SOLUTIONCOMMON_H
