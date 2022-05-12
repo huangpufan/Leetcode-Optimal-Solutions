c6cab45c6e4883dc245a6a0ee3c25f66b7db3b24
hpf <59730801@qq.com>
2022-05-12 10:32:36 +0800

    完成 0009 回文数判断 Optimal Solution

--- AllSolutions/Solution0009.h
+++ AllSolutions/Solution0009.h
@@ -20,15 +20,26 @@
 class Solution0009 {
 public:
     bool isPalindrome(int x) {
-
+        // 本题看起来的最优解就是翻转数字，这样不需要很多位存，然后只需要一个 int 空间
+        if (x < 0) {
+            return false;
+        }
+        const int inputX=x;
+        // Using int here got error.
+        long reverse = 0;
+        while (x > 0) {
+            reverse = reverse * 10 + (x % 10);
+            x /= 10;
+        }
+        return reverse == inputX;
     }
 };
 
 
 /*
  * 这道题是 PF-A01 做的第二道题目
- * 下面是我第一反应的做法，其实无可厚非，如果是往常的我应该直接就过了，但是因为是要做最优解
- * 一定要找最好的做法，去看了评论才懂，这道题优化的位置所在。这也是本项目的最大意义所在，打破自己的舒适区，去看看更强的人和做法。
+ * 下面是我第一反应的做法，其实无可厚非，如果是往常的我应该直接就过了，但是因为是要做最优解，害的看看评论
+ * 一定要找最好的做法，这也是本项目的最大意义所在，打破自己的舒适区，去看看更强的人和做法。
  */
 
 //bool isPalindrome(int x) {
