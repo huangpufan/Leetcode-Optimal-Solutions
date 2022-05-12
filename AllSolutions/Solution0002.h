33b90e870cdcb8ee4529af2e4809618c387c1dfe
hpf <59730801@qq.com>
2022-05-12 11:23:03 +0800

    完成 0002 链表逆序求和 自己版本的通过

--- AllSolutions/Solution0002.h
+++ AllSolutions/Solution0002.h
@@ -10,8 +10,54 @@
 class Solution0002 {
 
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+        ListNode *p1 = l1, *p2 = l2;
+        ListNode *res = nullptr;
+        ListNode *head = new ListNode(-1), *p3 = head;
 
+        int nextBit = 0;
+        int thisBit = 0;
+        while (p1 != nullptr && p2 != nullptr) {
+            thisBit = nextBit + p1->val + p2->val;
+            if (thisBit >= 10) {
+                nextBit = 1;
+                thisBit = thisBit % 10;
+            } else {
+                nextBit = 0;
+            }
+            p3->next = new ListNode(thisBit);
+            p3 = p3->next;
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+        while (p1 != nullptr) {
+            thisBit = p1->val + nextBit;
+            if (thisBit >= 10) {
+                thisBit %= 10;
+                nextBit = 1;
+            } else {
+                nextBit = 0;
+            }
+            p3->next = new ListNode(thisBit);
+            p3 = p3->next;
+            p1 = p1->next;
+        }
+        while (p2 != nullptr) {
+            thisBit = p2->val + nextBit;
+            if (thisBit >= 10) {
+                thisBit %= 10;
+                nextBit = 1;
+            } else {
+                nextBit = 0;
+            }
+            p3->next = new ListNode(thisBit);
+            p3 = p3->next;
+            p2 = p2->next;
+        }
+        if (nextBit != 0) {
+            p3->next = new ListNode( nextBit);
+        }
+        return head->next;
     }
 };
 
