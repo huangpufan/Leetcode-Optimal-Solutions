dac44b0492bd7bebf58a3718ae48d265690e39eb
hpf <59730801@qq.com>
2022-05-12 11:30:10 +0800

    完成 0002 链表逆序求和 代码结构最优化的修改

--- AllSolutions/Solution0002.h
+++ AllSolutions/Solution0002.h
@@ -12,13 +12,15 @@ class Solution0002 {
 public:
     ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
         ListNode *p1 = l1, *p2 = l2;
-        ListNode *res = nullptr;
         ListNode *head = new ListNode(-1), *p3 = head;
 
         int nextBit = 0;
         int thisBit = 0;
-        while (p1 != nullptr && p2 != nullptr) {
-            thisBit = nextBit + p1->val + p2->val;
+        while (p1 != nullptr || p2 != nullptr || nextBit != 0) {
+
+            // 草了，我是真的短。
+            thisBit = nextBit+((p1== nullptr)?0:p1->val)+((p2== nullptr)?0:p2->val);
+
             if (thisBit >= 10) {
                 nextBit = 1;
                 thisBit = thisBit % 10;
@@ -27,39 +29,65 @@ public:
             }
             p3->next = new ListNode(thisBit);
             p3 = p3->next;
-            p1 = p1->next;
-            p2 = p2->next;
-        }
-        while (p1 != nullptr) {
-            thisBit = p1->val + nextBit;
-            if (thisBit >= 10) {
-                thisBit %= 10;
-                nextBit = 1;
-            } else {
-                nextBit = 0;
+            if(p1!= nullptr){
+                p1=p1->next;
             }
-            p3->next = new ListNode(thisBit);
-            p3 = p3->next;
-            p1 = p1->next;
-        }
-        while (p2 != nullptr) {
-            thisBit = p2->val + nextBit;
-            if (thisBit >= 10) {
-                thisBit %= 10;
-                nextBit = 1;
-            } else {
-                nextBit = 0;
+            if(p2!= nullptr){
+                p2=p2->next;
             }
-            p3->next = new ListNode(thisBit);
-            p3 = p3->next;
-            p2 = p2->next;
-        }
-        if (nextBit != 0) {
-            p3->next = new ListNode( nextBit);
         }
         return head->next;
+
     }
 };
 
+ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+    ListNode *p1 = l1, *p2 = l2;
+    ListNode *head = new ListNode(-1), *p3 = head;
+
+    int nextBit = 0;
+    int thisBit = 0;
+    while (p1 != nullptr && p2 != nullptr) {
+        thisBit = nextBit + p1->val + p2->val;
+        if (thisBit >= 10) {
+            nextBit = 1;
+            thisBit = thisBit % 10;
+        } else {
+            nextBit = 0;
+        }
+        p3->next = new ListNode(thisBit);
+        p3 = p3->next;
+        p1 = p1->next;
+        p2 = p2->next;
+    }
+    while (p1 != nullptr) {
+        thisBit = p1->val + nextBit;
+        if (thisBit >= 10) {
+            thisBit %= 10;
+            nextBit = 1;
+        } else {
+            nextBit = 0;
+        }
+        p3->next = new ListNode(thisBit);
+        p3 = p3->next;
+        p1 = p1->next;
+    }
+    while (p2 != nullptr) {
+        thisBit = p2->val + nextBit;
+        if (thisBit >= 10) {
+            thisBit %= 10;
+            nextBit = 1;
+        } else {
+            nextBit = 0;
+        }
+        p3->next = new ListNode(thisBit);
+        p3 = p3->next;
+        p2 = p2->next;
+    }
+    if (nextBit != 0) {
+        p3->next = new ListNode(nextBit);
+    }
+    return head->next;
+}
 
 #endif //LEETCODE_OPTIMAL_SOLUTIONS_SOLUTION0002_H
