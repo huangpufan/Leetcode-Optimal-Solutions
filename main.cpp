d0110e45825f05d5bb99e9e6e3ae3de943597010
hpf <59730801@qq.com>
2022-05-15 20:46:34 +0800

    提交 53. 最大子数组和

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include "SolutionCommon.h"
-#include "AllSolutions/Solution0003.h"
+#include "AllSolutions/Solution0053.h"
 
 int main() {
-    string a="bbbbb",b="bada";
+    string a = "bbbbb", b = "bada";
     Solution s;
-    cout<<s.lengthOfLongestSubstring(a);
+    vector<int>aaa{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    cout<<s.maxSubArray(aaa);
     return 0;
 }
