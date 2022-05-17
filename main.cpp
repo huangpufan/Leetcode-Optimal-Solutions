fa5e869cb73364d85c6f89f0c09365e5e2996482
hpf <59730801@qq.com>
2022-05-17 23:15:23 +0800

    提交 19. 删除链表的倒数第 N 个结点 mid

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include "SolutionCommon.h"
-#include "AllSolutions/Solution0020.h"
+#include "AllSolutions/Solution0019.h"
 
 int main() {
     string a = "bbbbb", b = "bada";
     Solution s;
     vector<int>aaa{-2, 1, -3, 4, -1, 2, 1, -5, 4};
-    cout<<s.isValid("((");
+
+    ListNode bb(1);
+    s.removeNthFromEnd(&bb,1);
+
     return 0;
 }
