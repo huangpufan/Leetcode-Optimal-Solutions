1203e277f0546c1ede377d570602921df6ddd7cb
hpf <59730801@qq.com>
2022-05-18 23:57:20 +0800

    0237

---
+++ AllSolutions/Solution0237.h
@@ -0,0 +1,22 @@
+//
+// Created by hpf on 22-5-18.
+//
+
+#ifndef LEETCODE_OPTIMAL_SOLUTIONS_SOLUTION0237_H
+#define LEETCODE_OPTIMAL_SOLUTIONS_SOLUTION0237_H
+#include "../SolutionCommon.h"
+
+// 237. 删除链表中的节点
+//
+// 请编写一个函数，用于 删除单链表中某个特定节点 。在设计函数时需要注意，你无法访问链表的头节点 head ，只能直接访问 要被删除的节点 。
+//
+// 题目数据保证需要删除的节点 不是末尾节点 。
+
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+
+    }
+};
+#endif //LEETCODE_OPTIMAL_SOLUTIONS_SOLUTION0237_H
