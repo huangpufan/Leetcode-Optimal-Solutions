d33cf35c23c7f45f6cc9dcff967cbe3b154485c4
hpf <59730801@qq.com>
2022-05-19 07:51:44 +0800

    // 237. 删除链表中的节点 参透指针

--- AllSolutions/Solution0237.h
+++ AllSolutions/Solution0237.h
@@ -12,11 +12,31 @@
 //
 // 题目数据保证需要删除的节点 不是末尾节点 。
 
-
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-
+        *node = *(node->next);
     }
 };
+
+
+
+// 我参透了指针
+// 按理说，这里 node=node.next 就可以。但实际上不可以
+// 根本原因是，传入的是值变量（一级指针），而不是二级指针/一级指针引用。
+
 #endif //LEETCODE_OPTIMAL_SOLUTIONS_SOLUTION0237_H
+
+
+// 小猪做法，还搁着一个一个走呢
+    void deleteNode(ListNode* node) {
+        ListNode *p=node,*q= nullptr;
+        while(p!= nullptr&&p->next!= nullptr){
+            p->val=p->next->val;
+            q=p;
+            p=p->next;
+        }
+        if(q!= nullptr){
+            delete q;
+        }
+    }
