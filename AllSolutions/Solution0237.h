6e95e14a63c2c69fb64d388bcc2a1bdbebca6ec4
hpf <59730801@qq.com>
2022-05-19 23:17:27 +0800

    219. 存在重复元素 II ez

--- AllSolutions/Solution0237.h
+++ AllSolutions/Solution0237.h
@@ -6,7 +6,7 @@
 #define LEETCODE_OPTIMAL_SOLUTIONS_SOLUTION0237_H
 #include "../SolutionCommon.h"
 
-// 237. 删除链表中的节点
+/// 237. 删除链表中的节点
 //
 // 请编写一个函数，用于 删除单链表中某个特定节点 。在设计函数时需要注意，你无法访问链表的头节点 head ，只能直接访问 要被删除的节点 。
 //
---
+++ AllSolutions/Solution0242.h
@@ -0,0 +1,44 @@
+//
+// Created by hpf on 22-5-19.
+//
+
+#ifndef LEETCODE_OPTIMAL_SOLUTIONS_SOLUTION0242_H
+#define LEETCODE_OPTIMAL_SOLUTIONS_SOLUTION0242_H
+#include "../SolutionCommon.h"
+
+/// 242. 有效的字母异位词 ez
+
+// 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+
+// 注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int>map;
+        for(auto& c:s){
+            if(map.count(c)!=0){
+                ++map[c];
+            }else{
+                map[c]=1;
+            }
+        }
+        for(auto & c:t){
+            if(map.count(c)!=0){
+                --map[c];
+            }else{
+                return false;
+            }
+        }
+        for(auto& kv:map){
+            if(kv.second!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+// ezez
+
+#endif //LEETCODE_OPTIMAL_SOLUTIONS_SOLUTION0242_H
