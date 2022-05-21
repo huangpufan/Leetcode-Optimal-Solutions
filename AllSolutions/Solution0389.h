7a3581e3cc6906ff74686bfc1b234cbf8775ff35
hpf <59730801@qq.com>
2022-05-21 09:31:51 +0800

    389. 找不同 ez

---
+++ AllSolutions/Solution0389.h
@@ -0,0 +1,34 @@
+//
+// Created by hpf on 22-5-21.
+//
+
+#ifndef LEETCODE_OPTIMAL_SOLUTIONS_SOLUTION0389_H
+#define LEETCODE_OPTIMAL_SOLUTIONS_SOLUTION0389_H
+#include "../SolutionCommon.h"
+
+
+// 389. 找不同 ez
+
+// 给定两个字符串 s 和 t ，它们只包含小写字母。
+
+// 字符串 t 由字符串 s 随机重排，然后在随机位置添加一个字母。
+
+// 请找出在 t 中被添加的字母。
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int i=0;
+        for(auto &c:s){
+            i^=c;
+        }
+        for(auto&c:t){
+            i^=c;
+        }
+        return i;
+    }
+};
+
+// 猪猪题
+
+#endif //LEETCODE_OPTIMAL_SOLUTIONS_SOLUTION0389_H
