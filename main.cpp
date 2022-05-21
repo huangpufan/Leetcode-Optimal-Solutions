72c1382dcdf9e5d0c3e1dce2bf9f88d9aead632f
hpf <59730801@qq.com>
2022-05-21 09:44:28 +0800

    387. 字符串中的第一个唯一字符 ez

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include "SolutionCommon.h"
-#include "AllSolutions/Solution0191.h"
+#include "AllSolutions/Solution0387.h"
 
 int main() {
     string a = "bbbbb", b = "bada";
     Solution s;
-    s.hammingWeight(3);
+    s.firstUniqChar("lead");
     return 0;
 }
