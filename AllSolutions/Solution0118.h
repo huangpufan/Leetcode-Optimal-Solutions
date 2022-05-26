5002d2bc0e53ea7e55aef736d9fe2b1260f63c6e
hpf <59730801@qq.com>
2022-05-26 10:23:21 +0800

    LCS 01. 下载插件 ez

---
+++ AllSolutions/Solution0118.h
@@ -0,0 +1,24 @@
+//
+// Created by hpf on 22-5-26.
+//
+
+#ifndef LEETCODE_OPTIMAL_SOLUTIONS_SOLUTION0118_H
+#define LEETCODE_OPTIMAL_SOLUTIONS_SOLUTION0118_H
+#include "../SolutionCommon.h"
+
+
+// 118. 杨辉三角 ez
+
+// 给定一个非负整数 numRows，生成「杨辉三角」的前 numRows 行。
+//
+// 在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        int[]triangle=new int[numRows][];
+        vector<vector<int>>res;
+    }
+};
+
+#endif //LEETCODE_OPTIMAL_SOLUTIONS_SOLUTION0118_H
