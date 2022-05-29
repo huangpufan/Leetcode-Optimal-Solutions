1485290e1b0ee782163c452b4d3b5c3cdbd2b92d
hpf <59730801@qq.com>
2022-05-29 10:27:25 +0800

    63. 不同路径 II mid

--- AllSolutions/Solution0063.h
+++ AllSolutions/Solution0063.h
@@ -22,29 +22,55 @@ class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid) {
         int m = obstacleGrid.size(), n = obstacleGrid[0].size();
-        vector<vector<int>> routeCount(m , vector<int>(n , 0));
-        if (obstacleGrid[m - 1][n - 1] == 1||obstacleGrid[0][0]==1) {
+        vector<vector<int>> routeCount(m, vector<int>(n, 0));
+        if (obstacleGrid[m - 1][n - 1] == 1 || obstacleGrid[0][0] == 1) {
             return 0;
         }
-        for (int i = m - 1; m >= 0; m--) {
-            for (int j = n - 1; j >= 0; j--) {
-                if (obstacleGrid[i][j] == 1) {
-                    routeCount[i][j] = 0;
-                    continue;
-                }
-                if (i == m - 1 && j == n - 1) {
-                    routeCount[i][j] = 1;
-                } else if (i == m - 1) {
-                    routeCount[i][j] = routeCount[i][j + 1];
-                } else if (j == n - 1) {
-                    routeCount[i][j] = routeCount[i + 1][j];
-                } else {
-                    routeCount[i][j] = routeCount[i+1][j]+routeCount[i][j+1];
+        // 这是真滴棒，这个循环
+        for (int i = 0; i < m && obstacleGrid[i][0] == 0; ++i) {
+            routeCount[i][0] = 1;
+        }
+        for (int j = 0; j < n && obstacleGrid[0][j] == 0; ++j) {
+            routeCount[0][j] = 1;
+        }
+        for(int i=1;i<m;++i){
+            for(int j=1;j<n;++j){
+                if(obstacleGrid[i][j]==0){
+                    routeCount[i][j]=routeCount[i-1][j]+routeCount[i][j-1];
                 }
             }
         }
-        return routeCount[0][0];
+
+        return routeCount[m-1][n-1];
     }
 };
+
+// 开始的思路，每次的判断太多了，应该拿出来。
+
+int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid) {
+    int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+    vector<vector<int>> routeCount(m, vector<int>(n, 0));
+    if (obstacleGrid[m - 1][n - 1] == 1 || obstacleGrid[0][0] == 1) {
+        return 0;
+    }
+    for (int i = m - 1; m >= 0; m--) {
+        for (int j = n - 1; j >= 0; j--) {
+            if (obstacleGrid[i][j] == 1) {
+                routeCount[i][j] = 0;
+                continue;
+            }
+            if (i == m - 1 && j == n - 1) {
+                routeCount[i][j] = 1;
+            } else if (i == m - 1) {
+                routeCount[i][j] = routeCount[i][j + 1];
+            } else if (j == n - 1) {
+                routeCount[i][j] = routeCount[i + 1][j];
+            } else {
+                routeCount[i][j] = routeCount[i + 1][j] + routeCount[i][j + 1];
+            }
+        }
+    }
+    return routeCount[0][0];
+}
 
 #endif //LEETCODE_OPTIMAL_SOLUTIONS_SOLUTION0063_H
