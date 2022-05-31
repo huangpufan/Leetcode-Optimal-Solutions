1999b07686d64a2fdfc0e625dc07e55afe6f92a1
hpf <59730801@qq.com>
2022-05-31 09:46:06 +0800

    559. N 叉树的最大深度 ez

---
+++ AllSolutions/Solution0559.h
@@ -0,0 +1,59 @@
+//
+// Created by hpf on 22-5-31.
+//
+
+#ifndef LEETCODE_OPTIMAL_SOLUTIONS_SOLUTION0559_H
+#define LEETCODE_OPTIMAL_SOLUTIONS_SOLUTION0559_H
+
+#include "../SolutionCommon.h"
+
+// 559. N 叉树的最大深度 ez
+
+// 给定一个 N 叉树，找到其最大深度。
+
+// 最大深度是指从根节点到最远叶子节点的最长路径上的节点总数。
+
+// N 叉树输入按层序遍历序列化表示，每组子节点由空值分隔（请参见示例）。
+
+
+
+class Solution {
+public:
+    int maxDepth(Node* root) {
+       // 迭代还是。
+       if(root== nullptr){
+           return 0;
+       }
+       int depth=0;
+       queue<Node*>layerNodes;
+       layerNodes.push(root);
+
+       while(layerNodes.empty()== false){
+            ++depth;
+            int thisLayerCount=layerNodes.size();
+            for(int i=0;i<thisLayerCount;++i){
+                auto node=layerNodes.front();
+                layerNodes.pop();
+                for(const auto &child:node->children){
+                    layerNodes.push(child);
+                }
+            }
+       }
+       return depth;
+    }
+};
+
+// 猪猪递归
+
+int maxDepth(Node* root) {
+    if(root== nullptr){
+        return 0;
+    }
+    auto max=0;
+    for(auto &child:root->children){
+        auto tmp= maxDepth(child);
+        max=(max<tmp)?tmp: max;
+    }
+    return max+1;
+}
+#endif //LEETCODE_OPTIMAL_SOLUTIONS_SOLUTION0559_H
